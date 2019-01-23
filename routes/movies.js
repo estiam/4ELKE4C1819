b157eb2db65ff78a2813b7cb3ac200e51446a468
Sion GENDERS <sion.genders@estiam.com>
2019-01-23 17:30:06 +0100

    Create and Edit

--- routes/movies.js
+++ routes/movies.js
@@ -3,15 +3,55 @@ var router = express.Router();
 const mongoose = require('mongoose');
 const Movies = mongoose.model('Movie');
 
+// https://github.com/estiam/4ELKE4C1819
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   Movies.find({}, (err, items) => {
-    if(err)
+    if (err)
       return res.send(err);
 
     return res.render('moviesList', { movies: items });
 
   });
 });
+
+router.get('/create', (req, res, next) => {
+  res.render('create');
+});
+
+router.post('/create', (req, res, next) => {
+  const movie = req.body;
+
+  Movies.create(movie, (err, item) => {
+    if (err)
+      return res.send(err);
+
+    return res.redirect('/movies');
+  });
+});
+router.get('/edit/:id', (req, res, next) => {
+  Movies.findById(req.params.id, (err, item) => {
+    if(err) return res.send(err);
+
+    res.render('edit', { movie: item });
+  });
+});
+
+router.post('/edit/:id', (req, res, next) => {
+  const movie = req.body;
+
+  Movies.findByIdAndUpdate(req.params.id, movie, (err, item) => {
+    if (err)
+      return res.send(err);
+
+    return res.redirect('/movies');
+  });
+});
+
+
+
+
 
 module.exports = router;
